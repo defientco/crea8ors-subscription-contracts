5df034679b75a22490a1e60848fc5d49327705cd
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-01 22:40:33 +0500

    feat: modularize Subscription contract style: add sections feat: add function overloads for subscription with mint variants docs: add natspec

--- contracts/Subscription.sol
+++ contracts/Subscription.sol
@@ -1,75 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { IERC5643 } from "./interfaces/IERC5643.sol";
+import { ERC20TransferHelper } from "./libraries/ERC20TransferHelper.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import { console2 } from "forge-std/console2.sol";
-import { PaymentSystem } from "./abstracts/PaymentSystem.sol";
-import { ERC20TransferHelper } from "./libraries/ERC20TransferHelper.sol";
+import { IERC5643 } from "./interfaces/IERC5643.sol";
 import { ISubscription } from "./interfaces/ISubscription.sol";
+import { ERC5643 } from "./abstracts/ERC5643.sol";
 
-error RenewalTooShort();
-error RenewalTooLong();
-error InsufficientPayment();
-error SubscriptionNotRenewable();
-error InvalidTokenId();
-error CallerNotOwnerNorApproved();
+contract Subscription is ISubscription, ERC5643 {
+    /*//////////////////////////////////////////////////////////////
+                            PUBLIC CONSTANTS
+    //////////////////////////////////////////////////////////////*/
 
-error CallerNotERC721();
-error ReceiveNotAllowed();
-error CalledWithOutData();
-
-error InvalidSubscription();
-error Access_OnlyMinter();
-
-contract Subscription is IERC5643, ISubscription, PaymentSystem {
     uint64 public constant DEFAULT_SUBSCRIPTION_DURATION = 365 days;
-    mapping(uint256 tokenId => uint64 expiresAt) private _expirations;
 
-    uint64 public minRenewalDuration;
-    uint64 public maxRenewalDuration; // 0 value means lifetime extension
+    /*//////////////////////////////////////////////////////////////
+                             PRIVATE STORAGE
+    //////////////////////////////////////////////////////////////*/
 
     bool private _renewable;
 
-    constructor(address _cre8orsNFT, address _minter) PaymentSystem(_cre8orsNFT, _minter) { }
+    /*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
 
+    constructor(address _cre8orsNFT, address _minter) ERC5643(_cre8orsNFT, _minter) { }
+
+    /*//////////////////////////////////////////////////////////////
+                     USER-FACING CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ISubscription
     function isSubscriptionValid(uint256 tokenId) public view override returns (bool) {
-        return _expirations[tokenId] > block.timestamp;
+        return expiresAt(tokenId) > block.timestamp;
     }
 
-    // crea8ors nft will call validateSubscription in ownerOf and then super.ownerOf()
+    /// @inheritdoc ISubscription
     function validateSubscription(uint256 tokenId) public view override returns (bool) {
-        bool flag = isSubscriptionValid(tokenId);
+        bool isValid = isSubscriptionValid(tokenId);
 
-        if (flag) {
+        if (!isValid) {
             revert InvalidSubscription();
         }
 
-        return flag;
+        return isValid;
     }
 
-    function renewSubscription(uint256 tokenId, uint64 duration) external payable virtual override {
-        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
-            revert CallerNotOwnerNorApproved();
-        }
+    /*//////////////////////////////////////////////////////////////
+                   USER-FACING NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
-        // check duration
-        if (duration < minRenewalDuration) {
-            revert RenewalTooShort();
-        } else if (maxRenewalDuration != 0 && duration > maxRenewalDuration) {
-            revert RenewalTooLong();
-        }
-
-        // check msg.value
-        if (msg.value < _getRenewalPrice(tokenId, duration)) {
-            revert InsufficientPayment();
-        }
-
-        // extend subscription
-        _updateSubscriptionExpiration(tokenId, duration);
-    }
-
+    /// @inheritdoc ISubscription
     function renewSubscriptionWithERC20Payment(
         uint256 tokenId,
         uint64 duration,
@@ -79,52 +61,17 @@ contract Subscription is IERC5643, ISubscription, PaymentSystem {
         payable
         override
     {
-        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
-            revert CallerNotOwnerNorApproved();
-        }
+        _validateCallerAsOwnerOrApproved({ caller: msg.sender, tokenId: tokenId });
 
-        // check duration
-        if (duration < minRenewalDuration) {
-            revert RenewalTooShort();
-        } else if (maxRenewalDuration != 0 && duration > maxRenewalDuration) {
-            revert RenewalTooLong();
-        }
-
-        // ERC-20 Specific pre-flight checks
-        uint256 tokensQtyToTransfer = chargeAmountForERC20(erc20) * duration;
-        IERC20 payableToken = IERC20(erc20);
-
-        if (payableToken.balanceOf(msg.sender) < tokensQtyToTransfer) revert ERC20InsufficientBalance();
-        if (payableToken.allowance(msg.sender, address(this)) < tokensQtyToTransfer) {
-            revert ERC20InsufficientAllowance();
-        }
-
-        ERC20TransferHelper.safeTransferFrom(IERC20(erc20), msg.sender, address(this), tokensQtyToTransfer);
-
-        // extend subscription
-        _updateSubscriptionExpiration(tokenId, duration);
+        _handleERC20PaymentSubscriptionUpdate(tokenId, duration, erc20);
     }
 
-    function cancelSubscription(uint256 tokenId) external payable virtual override {
-        if (!_isApprovedOrOwner(msg.sender, tokenId)) {
-            revert CallerNotOwnerNorApproved();
-        }
+    /*//////////////////////////////////////////////////////////////
+                    ONLY-ADMIN NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
-        delete _expirations[tokenId];
-
-        emit SubscriptionUpdate(tokenId, 0);
-    }
-
-    function expiresAt(uint256 tokenId) external view virtual override returns (uint64) {
-        return _expirations[tokenId];
-    }
-
-    function isRenewable(uint256 tokenId) external view virtual override returns (bool) {
-        return _isRenewable(tokenId);
-    }
-
-    function setRenewable(bool renewable_) external onlyAdmin {
-        _renewable = renewable_;
+    function setRenewable(bool renewable) external onlyAdmin {
+        _renewable = renewable;
     }
 
     function setMinRenewalDuration(uint64 duration) external onlyAdmin {
@@ -135,34 +82,126 @@ contract Subscription is IERC5643, ISubscription, PaymentSystem {
         _setMaximumRenewalDuration(duration);
     }
 
-    /**
-     * @dev See {IERC165-supportsInterface}.
-     */
-    function supportsInterface(bytes4 interfaceId) public view virtual returns (bool) {
-        return interfaceId == type(IERC5643).interfaceId || IERC721(cre8orsNFT).supportsInterface(interfaceId);
-    }
+    /*//////////////////////////////////////////////////////////////
+                   ONLY-MINTER NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
-    function updateSubscriptionForFree(uint256 tokenId, uint64 duration) external onlyMinter {
+    /*//////////   updateSubscriptionForFree variants   //////////*/
+
+    /// @inheritdoc ISubscription
+    function updateSubscriptionForFree(uint256 tokenId, uint64 duration) external override onlyMinter {
+        _validateDurationBetweenMinAndMax(duration);
         _updateSubscriptionExpiration(tokenId, duration);
     }
 
+    /// @inheritdoc ISubscription
+    function updateSubscriptionForFree(uint256[] calldata tokenIds, uint64 duration) external override onlyMinter {
+        uint256 tokenId;
+
+        for (uint256 i = 0; i < tokenIds.length;) {
+            tokenId = tokenIds[i];
+
+            _validateDurationBetweenMinAndMax(duration);
+            _updateSubscriptionExpiration(tokenId, duration);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @inheritdoc ISubscription
+    function updateSubscriptionForFree(
+        uint256[] calldata tokenIds,
+        uint64[] calldata durations
+    )
+        external
+        override
+        onlyMinter
+    {
+        if (tokenIds.length != durations.length) revert LengthMismatch();
+
+        uint256 tokenId;
+        uint64 duration;
+
+        for (uint256 i = 0; i < tokenIds.length;) {
+            tokenId = tokenIds[i];
+            duration = durations[i];
+
+            _validateDurationBetweenMinAndMax(duration);
+            _updateSubscriptionExpiration(tokenId, duration);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /*//////////////   updateSubscription variants   /////////////*/
+
+    /// @inheritdoc ISubscription
     function updateSubscription(uint256 tokenId, uint64 duration) external payable override onlyMinter {
-        // check duration
-        if (duration < minRenewalDuration) {
-            revert RenewalTooShort();
-        } else if (maxRenewalDuration != 0 && duration > maxRenewalDuration) {
-            revert RenewalTooLong();
-        }
-
-        // check msg.value
-        if (msg.value < _getRenewalPrice(tokenId, duration)) {
-            revert InsufficientPayment();
-        }
-
-        // extend subscription
-        _updateSubscriptionExpiration(tokenId, duration);
+        _handleNativePaymentSubscriptionUpdate(tokenId, msg.value, duration);
     }
 
+    /// @inheritdoc ISubscription
+    function updateSubscription(uint256[] calldata tokenIds, uint64 duration) external payable override onlyMinter {
+        uint256 tokenId;
+
+        for (uint256 i = 0; i < tokenIds.length;) {
+            tokenId = tokenIds[i];
+
+            _handleNativePaymentSubscriptionUpdate(tokenId, msg.value, duration);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @inheritdoc ISubscription
+    function updateSubscription(
+        uint256[] calldata tokenIds,
+        uint64[] calldata durations
+    )
+        external
+        payable
+        override
+        onlyMinter
+    {
+        if (tokenIds.length != durations.length) revert LengthMismatch();
+
+        uint256 totalDuration; // to prevent overflow
+        for (uint256 i = 0; i < durations.length;) {
+            totalDuration += durations[i];
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        _validateRenewalPrice(msg.value, uint64(totalDuration));
+
+        uint256 tokenId;
+        uint64 duration;
+
+        for (uint256 i = 0; i < tokenIds.length;) {
+            tokenId = tokenIds[i];
+            duration = durations[i];
+
+            _validateDurationBetweenMinAndMax(duration);
+
+            _updateSubscriptionExpiration(tokenId, duration);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /*//////   updateSubscriptionWithERC20Payment variants   /////*/
+
+    /// @inheritdoc ISubscription
     function updateSubscriptionWithERC20Payment(
         uint256 tokenId,
         uint64 duration,
@@ -172,90 +211,113 @@ contract Subscription is IERC5643, ISubscription, PaymentSystem {
         override
         onlyMinter
     {
-        // check duration
-        if (duration < minRenewalDuration) {
-            revert RenewalTooShort();
-        } else if (maxRenewalDuration != 0 && duration > maxRenewalDuration) {
-            revert RenewalTooLong();
+        _handleERC20PaymentSubscriptionUpdate(tokenId, duration, erc20);
+    }
+
+    /// @inheritdoc ISubscription
+    function updateSubscriptionWithERC20Payment(
+        uint256[] calldata tokenIds,
+        uint64 duration,
+        address erc20
+    )
+        external
+        override
+        onlyMinter
+    {
+        uint256 tokenId;
+
+        for (uint256 i = 0; i < tokenIds.length;) {
+            tokenId = tokenIds[i];
+
+            _handleERC20PaymentSubscriptionUpdate(tokenId, duration, erc20);
+
+            unchecked {
+                ++i;
+            }
+        }
+    }
+
+    /// @inheritdoc ISubscription
+    function updateSubscriptionWithERC20Payment(
+        uint256[] calldata tokenIds,
+        uint64[] calldata durations,
+        address erc20
+    )
+        external
+        override
+        onlyMinter
+    {
+        if (tokenIds.length != durations.length) revert LengthMismatch();
+
+        uint256 totalDuration; // to prevent overflow
+        for (uint256 i = 0; i < durations.length;) {
+            totalDuration += durations[i];
+
+            unchecked {
+                ++i;
+            }
         }
 
         // ERC-20 Specific pre-flight checks
-        uint256 tokensQtyToTransfer = chargeAmountForERC20(erc20) * duration;
-        IERC20 payableToken = IERC20(erc20);
+        _handleERC20Payment(uint64(totalDuration), erc20);
 
-        if (payableToken.balanceOf(msg.sender) < tokensQtyToTransfer) revert ERC20InsufficientBalance();
-        if (payableToken.allowance(msg.sender, address(this)) < tokensQtyToTransfer) {
-            revert ERC20InsufficientAllowance();
+        uint256 tokenId;
+        uint64 duration;
+
+        for (uint256 i = 0; i < tokenIds.length;) {
+            tokenId = tokenIds[i];
+            duration = durations[i];
+
+            _validateDurationBetweenMinAndMax(duration);
+
+            _updateSubscriptionExpiration(tokenId, duration);
+
+            unchecked {
+                ++i;
+            }
         }
+    }
 
-        ERC20TransferHelper.safeTransferFrom(IERC20(erc20), msg.sender, address(this), tokensQtyToTransfer);
+    /*//////////////////////////////////////////////////////////////
+                       INTERNAL CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ERC5643
+    function _isRenewable() internal view override returns (bool) {
+        return _renewable;
+    }
+
+    /// @inheritdoc ERC5643
+    function _getRenewalPrice(uint64 duration) internal view override returns (uint256) {
+        return duration * nativeCurrencyPrice;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                     INTERNAL NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _handleERC20PaymentSubscriptionUpdate(uint256 tokenId, uint64 duration, address erc20) internal {
+        _validateDurationBetweenMinAndMax(duration);
+
+        // ERC-20 Specific pre-flight checks
+        _handleERC20Payment(duration, erc20);
 
         // extend subscription
         _updateSubscriptionExpiration(tokenId, duration);
     }
 
-    /// @notice Updates the expiration timestamp for a subscription represented by the given `tokenId`.
-    /// @dev this function won't check that the tokenId is valid, responsibility is delegated to the caller.
-    /// @param tokenId The unique identifier of the subscription token.
-    /// @param duration The duration (in seconds) to extend the subscription from the current timestamp.
-    function _updateSubscriptionExpiration(uint256 tokenId, uint64 duration) internal virtual {
-        uint64 currentExpiration = _expirations[tokenId];
-        uint64 newExpiration;
+    function _handleERC20Payment(uint64 duration, address erc20) internal {
+        address msgSender = msg.sender;
+        address addressThis = address(this);
 
-        // Check if the current subscription is new or has expired
-        if ((currentExpiration == 0) || (currentExpiration < block.timestamp)) {
-            newExpiration = uint64(block.timestamp) + duration;
-        } else {
-            // If current subscription not expired (extend)
-            if (!_isRenewable(tokenId)) {
-                revert SubscriptionNotRenewable();
-            }
-            newExpiration = currentExpiration + duration;
+        uint256 tokensQtyToTransfer = chargeAmountForERC20(erc20) * duration;
+        IERC20 payableToken = IERC20(erc20);
+
+        if (payableToken.balanceOf(msgSender) < tokensQtyToTransfer) revert ERC20InsufficientBalance();
+        if (payableToken.allowance(msgSender, addressThis) < tokensQtyToTransfer) {
+            revert ERC20InsufficientAllowance();
         }
 
-        _expirations[tokenId] = newExpiration;
-
-        emit SubscriptionUpdate(tokenId, newExpiration);
-    }
-
-    /**
-     * @dev Internal function to determine renewability. Implementing contracts
-     * should override this function if renewabilty should be disabled for all or
-     * some tokens.
-     */
-    function _isRenewable(uint256 /*tokenId*/ ) internal view virtual returns (bool) {
-        return _renewable;
-    }
-
-    /**
-     * @dev Gets the price to renew a subscription for `duration` seconds for
-     * a given tokenId. This should be overridden in implementing contracts.
-     */
-    // solhint-disable-next-line
-    function _getRenewalPrice(uint256 tokenId, uint64 duration) internal view virtual returns (uint256) {
-        return duration * nativeCurrencyPrice;
-    }
-
-    /**
-     * @dev Internal function to set the minimum renewal duration.
-     */
-    function _setMinimumRenewalDuration(uint64 duration) internal virtual {
-        minRenewalDuration = duration;
-    }
-
-    /**
-     * @dev Internal function to set the maximum renewal duration.
-     */
-    function _setMaximumRenewalDuration(uint64 duration) internal virtual {
-        maxRenewalDuration = duration;
-    }
-
-    // if crea8ors has an `isApprovedOrOwner` then this can be removed
-    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
-        address owner = IERC721(cre8orsNFT).ownerOf(tokenId);
-        return (
-            spender == owner || IERC721(cre8orsNFT).isApprovedForAll(owner, spender)
-                || IERC721(cre8orsNFT).getApproved(tokenId) == spender
-        );
+        ERC20TransferHelper.safeTransferFrom(IERC20(erc20), msgSender, addressThis, tokensQtyToTransfer);
     }
 }
--- contracts/interfaces/ISubscription.sol
+++ contracts/interfaces/ISubscription.sol
@@ -4,35 +4,22 @@ pragma solidity ^0.8.19;
 /// @title ISubscription
 /// @dev Interface for managing subscriptions to NFTs.
 interface ISubscription {
-    /// @notice Extends the subscription for the given `tokenId` with a specified `duration` for free.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenId The unique identifier of the NFT token to be subscribed.
-    /// @param duration The duration (in seconds) to extend the subscription for.
-    function updateSubscriptionForFree(uint256 tokenId, uint64 duration) external;
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
 
-    /// @notice Extends the subscription for the given `tokenId` with a specified `duration`, using native currency as
-    /// payment.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenId The unique identifier of the NFT token to be subscribed.
-    /// @param duration The duration (in seconds) to extend the subscription for.
-    function updateSubscription(uint256 tokenId, uint64 duration) external payable;
+    /// @notice The subscription associated with the provided token ID is invalid or has expired.
+    error InvalidSubscription();
 
-    /// @notice Extends the subscription for the given `tokenId` with a specified `duration`, using the accepted ERC20
-    /// token as payment.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenId The unique identifier of the NFT token to be subscribed.
-    /// @param duration The duration (in seconds) to extend the subscription for.
-    /// @param erc20 The address of the ERC20 token to be used for payment.
-    function updateSubscriptionWithERC20Payment(uint256 tokenId, uint64 duration, address erc20) external;
+    error LengthMismatch();
 
-    /// @notice Renews the subscription to an NFT with the specified `tokenId`, using the accepted ERC20 token as the
-    /// payment method.
-    /// @dev This function can be called by NFT owners to renew their subscriptions using ERC20 tokens as payment.
-    /// Throws if `tokenId` is not a valid NFT.
-    /// @param tokenId The unique identifier of the NFT token to be renewed.
-    /// @param duration The duration (in seconds) to extend the subscription for.
-    /// @param erc20 The address of the ERC20 token to be used for payment.
-    function renewSubscriptionWithERC20Payment(uint256 tokenId, uint64 duration, address erc20) external payable;
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /*//////////////////////////////////////////////////////////////
+                           CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /// @notice Returns whether the subscription for the given `tokenId` is valid.
     /// @param tokenId The unique identifier of the NFT token.
@@ -44,4 +31,91 @@ interface ISubscription {
     /// @param tokenId The unique identifier of the NFT token.
     /// @return A boolean indicating if the subscription is valid.
     function validateSubscription(uint256 tokenId) external view returns (bool);
+
+    /*//////////////////////////////////////////////////////////////
+                         NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Renews the subscription to an NFT with the specified `tokenId`, using the accepted ERC20 token as the
+    /// payment method.
+    /// @dev This function can be called by NFT owners to renew their subscriptions using ERC20 tokens as payment.
+    /// Throws if `tokenId` is not a valid NFT.
+    /// @param tokenId The unique identifier of the NFT token to be renewed.
+    /// @param duration The duration (in seconds) to extend the subscription for.
+    /// @param erc20 The address of the ERC20 token to be used for payment.
+    function renewSubscriptionWithERC20Payment(uint256 tokenId, uint64 duration, address erc20) external payable;
+
+    /*//////////   updateSubscriptionForFree variants   //////////*/
+
+    /// @notice Extends the subscription for the given `tokenId` with a specified `duration` for free.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenId The unique identifier of the NFT token to be subscribed.
+    /// @param duration The duration (in seconds) to extend the subscription for.
+    function updateSubscriptionForFree(uint256 tokenId, uint64 duration) external;
+
+    /// @notice Extends the subscription for the given `tokenIds` with a specified `duration` for free.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param duration The duration (in seconds) to extend the subscription for.
+    function updateSubscriptionForFree(uint256[] calldata tokenIds, uint64 duration) external;
+
+    /// @notice Extends the subscriptions for the given `tokenIds` with specified `durations` for free.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param durations An array of durations (in seconds) for which the subscriptions are to be extended.
+    function updateSubscriptionForFree(uint256[] calldata tokenIds, uint64[] calldata durations) external;
+
+    /*//////////////   updateSubscription variants   /////////////*/
+
+    /// @notice Extends the subscription for the given `tokenId` with a specified `duration`, using native currency as
+    /// payment.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenId The unique identifier of the NFT token to be subscribed.
+    /// @param duration The duration (in seconds) to extend the subscription for.
+    function updateSubscription(uint256 tokenId, uint64 duration) external payable;
+
+    /// @notice Extends the subscription for the given `tokenIds` with a specified `duration`, using native currency as
+    /// payment.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param duration The duration (in seconds) to extend the subscription for.
+    function updateSubscription(uint256[] calldata tokenIds, uint64 duration) external payable;
+
+    /// @notice Extends the subscription for the given `tokenIds` with specified `durations`, using native currency as
+    /// payment.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param durations An array of durations (in seconds) for which the subscriptions are to be extended.
+    function updateSubscription(uint256[] calldata tokenIds, uint64[] calldata durations) external payable;
+
+    /*//////   updateSubscriptionWithERC20Payment variants   /////*/
+
+    /// @notice Extends the subscription for the given `tokenId` with a specified `duration`, using the accepted ERC20
+    /// token as payment.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenId The unique identifier of the NFT token to be subscribed.
+    /// @param duration The duration (in seconds) to extend the subscription for.
+    /// @param erc20 The address of the ERC20 token to be used for payment.
+    function updateSubscriptionWithERC20Payment(uint256 tokenId, uint64 duration, address erc20) external;
+
+    /// @notice Extends the subscription for the given `tokenIds` with a specified `duration`, using the accepted ERC20
+    /// token as payment.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param duration The duration (in seconds) to extend the subscription for.
+    /// @param erc20 The address of the ERC20 token to be used for payment.
+    function updateSubscriptionWithERC20Payment(uint256[] calldata tokenIds, uint64 duration, address erc20) external;
+
+    /// @notice Extends the subscription for the given `tokenIds` with a specified `durations`, using the accepted ERC20
+    /// token as payment.
+    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
+    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param durations An array of durations (in seconds) for which the subscriptions are to be extended.
+    /// @param erc20 The address of the ERC20 token to be used for payment.
+    function updateSubscriptionWithERC20Payment(
+        uint256[] calldata tokenIds,
+        uint64[] calldata durations,
+        address erc20
+    )
+        external;
 }
