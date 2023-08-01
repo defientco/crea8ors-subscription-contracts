55469fe8156a82e94c5308d2ec018b6ef7d41a60
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-01 22:38:57 +0500

    feat: add errors style: add sections for readability

--- contracts/interfaces/IERC5643.sol
+++ contracts/interfaces/IERC5643.sol
@@ -1,22 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-// https://eips.ethereum.org/EIPS/eip-5643
+/// @title IERC5643
+/// @notice https://eips.ethereum.org/EIPS/eip-5643
 interface IERC5643 {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The function caller is neither the owner of the token nor approved to perform the operation.
+    error CallerNotOwnerNorApproved();
+
+    /// @notice The duration provided for renewal is too short to extend the subscription.
+    error RenewalTooShort();
+
+    /// @notice The duration provided for renewal exceeds the allowed maximum for subscription extension.
+    error RenewalTooLong();
+
+    /// @notice The payment received for the subscription renewal is insufficient.
+    error InsufficientPayment();
+
+    /// @notice The subscription associated with the token is not renewable and cannot be extended.
+    error SubscriptionNotRenewable();
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
     /// @notice Emitted when a subscription expiration changes
     /// @dev When a subscription is canceled, the expiration value should also be 0.
     event SubscriptionUpdate(uint256 indexed tokenId, uint64 expiration);
 
-    /// @notice Renews the subscription to an NFT
-    /// Throws if `tokenId` is not a valid NFT
-    /// @param tokenId The NFT to renew the subscription for
-    /// @param duration The number of seconds to extend a subscription for
-    function renewSubscription(uint256 tokenId, uint64 duration) external payable;
-
-    /// @notice Cancels the subscription of an NFT
-    /// @dev Throws if `tokenId` is not a valid NFT
-    /// @param tokenId The NFT to cancel the subscription for
-    function cancelSubscription(uint256 tokenId) external payable;
+    /*//////////////////////////////////////////////////////////////
+                           CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     /// @notice Gets the expiration date of a subscription
     /// @dev Throws if `tokenId` is not a valid NFT
@@ -29,4 +46,19 @@ interface IERC5643 {
     /// @param tokenId The NFT to get the expiration date of
     /// @return The renewability of a the subscription
     function isRenewable(uint256 tokenId) external view returns (bool);
+
+    /*//////////////////////////////////////////////////////////////
+                         NON-CONSTANT FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Renews the subscription to an NFT
+    /// Throws if `tokenId` is not a valid NFT
+    /// @param tokenId The NFT to renew the subscription for
+    /// @param duration The number of seconds to extend a subscription for
+    function renewSubscription(uint256 tokenId, uint64 duration) external payable;
+
+    /// @notice Cancels the subscription of an NFT
+    /// @dev Throws if `tokenId` is not a valid NFT
+    /// @param tokenId The NFT to cancel the subscription for
+    function cancelSubscription(uint256 tokenId) external payable;
 }
