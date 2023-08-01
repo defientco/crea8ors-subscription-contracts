75ad270c1a2cd71acac55039d8eefa4b42bcd763
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-01 17:14:28 +0500

    feat: add mock nft contract
    Mock NFT contract mimicks cre8ors contract and overrides ownerOf function to call subscription contract externally and validate subscription status
    Setter functions must be only access controlled by onlyAdmin
    
    Refs: #10

---
+++ contracts/mocks/MockNFT.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import { ISubscription } from "../interfaces/ISubscription.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IMockNFT {
+    function mint(address to, uint256 tokenId) external returns (uint256);
+    function burn(uint256 tokenId) external returns (uint256);
+}
+
+// cre8ors nft
+contract MockNFT is Ownable, ERC721 {
+    // REQUIRED
+    address public subscription;
+    bool public isSubscriptionEnabled;
+
+    uint256 public totalSupply;
+
+    constructor(address _subscription) ERC721("", "") {
+        subscription = _subscription;
+    }
+
+    function mint(address to, uint256 tokenId) public onlyOwner returns (uint256) {
+        _mint(to, tokenId);
+        totalSupply++;
+        return tokenId;
+    }
+
+    function burn(uint256 tokenId) public onlyOwner returns (uint256) {
+        _burn(tokenId);
+        totalSupply--;
+        return tokenId;
+    }
+
+    // REQUIRED
+    // override ownerOf
+    function ownerOf(uint256 tokenId) public view override returns (address) {
+        // external call to subscription if it is enabled and present
+        if (isSubscriptionEnabled && subscription != address(0)) {
+            // handles revert for us
+            ISubscription(subscription).validateSubscription(tokenId);
+        }
+        return super.ownerOf(tokenId);
+    }
+
+    // ONLY_ADMIN REQUIRED
+    function setSubscription(address s) external onlyOwner {
+        subscription = s;
+    }
+
+    // ONLY_ADMIN REQUIRED
+    function toggleSubscription() external onlyOwner {
+        isSubscriptionEnabled = !isSubscriptionEnabled;
+    }
+}
