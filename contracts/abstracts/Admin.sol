54b176cf2cbba4ff337adcfc3ad46e1ea7ac99b1
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-02 16:25:39 +0500

    refactor: remove Minter contract feat: add onlyRoleOrAdmin modifier in Admin abstract contract

--- contracts/abstracts/Admin.sol
+++ contracts/abstracts/Admin.sol
@@ -2,23 +2,40 @@
 pragma solidity ^0.8.19;
 
 import { IERC721Drop } from "@crea8ors/interfaces/IERC721Drop.sol";
+import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
 
 /// @title Admin
-/// @notice An abstract contract that restricts access to only the admin.
+/// @notice An abstract contract with access control functionality.
 abstract contract Admin {
-    /// @notice Only allow for users with admin access
-    modifier onlyAdmin(address _target) {
-        if (!isAdmin(_target, msg.sender)) {
+    /// @notice Access control roles
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER");
+
+    /// @notice Modifier to allow only users with admin access
+    /// @param target The address of the contract implementing the access control
+    modifier onlyAdmin(address target) {
+        if (!isAdmin(target, msg.sender)) {
             revert IERC721Drop.Access_OnlyAdmin();
         }
 
         _;
     }
 
+    /// @notice Modifier to allow only a given role or admin access
+    /// @param target The address of the contract implementing the access control
+    /// @param role The role to check for alongside the admin role
+    modifier onlyRoleOrAdmin(address target, bytes32 role) {
+        if (!isAdmin(target, msg.sender) && !IAccessControl(target).hasRole(role, msg.sender)) {
+            revert IERC721Drop.Access_MissingRoleOrAdmin(role);
+        }
+
+        _;
+    }
+
     /// @notice Getter for admin role associated with the contract to handle minting
-    /// @param user user address
-    /// @return boolean if address is admin
-    function isAdmin(address _target, address user) public view returns (bool) {
-        return IERC721Drop(_target).isAdmin(user);
+    /// @param target The address of the contract implementing the access control
+    /// @param user The address to check for admin access
+    /// @return Whether the address has admin access or not
+    function isAdmin(address target, address user) public view returns (bool) {
+        return IERC721Drop(target).isAdmin(user);
     }
 }
