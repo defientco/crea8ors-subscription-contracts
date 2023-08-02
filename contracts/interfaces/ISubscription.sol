4e4efd3249731f3a729d1cad704bbe6745c0c32f
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-02 21:24:10 +0500

    refactor: remove erc20 functions feat: add target mechanism perf: function params use two slots refactor: remove durations[] implementation functions

--- contracts/interfaces/ISubscription.sol
+++ contracts/interfaces/ISubscription.sol
@@ -38,86 +38,37 @@ interface ISubscription {
                          NON-CONSTANT FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
-    /// @notice Renews the subscription to an NFT with the specified `tokenId`, using the accepted ERC20 token as the
-    /// payment method.
-    /// @dev This function can be called by NFT owners to renew their subscriptions using ERC20 tokens as payment.
-    /// Throws if `tokenId` is not a valid NFT.
-    /// @param tokenId The unique identifier of the NFT token to be renewed.
-    /// @param duration The duration (in seconds) to extend the subscription for.
-    /// @param erc20 The address of the ERC20 token to be used for payment.
-    function renewSubscriptionWithERC20Payment(uint256 tokenId, uint64 duration, address erc20) external payable;
-
     /*//////////   updateSubscriptionForFree variants   //////////*/
 
     /// @notice Extends the subscription for the given `tokenId` with a specified `duration` for free.
     /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenId The unique identifier of the NFT token to be subscribed.
+    /// @param target The address of the contract implementing the access control
     /// @param duration The duration (in seconds) to extend the subscription for.
-    function updateSubscriptionForFree(uint256 tokenId, uint64 duration) external;
+    /// @param tokenId The unique identifier of the NFT token to be subscribed.
+    function updateSubscriptionForFree(address target, uint64 duration, uint256 tokenId) external;
 
     /// @notice Extends the subscription for the given `tokenIds` with a specified `duration` for free.
     /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param target The address of the contract implementing the access control
     /// @param duration The duration (in seconds) to extend the subscription for.
-    function updateSubscriptionForFree(uint256[] calldata tokenIds, uint64 duration) external;
-
-    /// @notice Extends the subscriptions for the given `tokenIds` with specified `durations` for free.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
     /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
-    /// @param durations An array of durations (in seconds) for which the subscriptions are to be extended.
-    function updateSubscriptionForFree(uint256[] calldata tokenIds, uint64[] calldata durations) external;
+    function updateSubscriptionForFree(address target, uint64 duration, uint256[] calldata tokenIds) external;
 
     /*//////////////   updateSubscription variants   /////////////*/
 
     /// @notice Extends the subscription for the given `tokenId` with a specified `duration`, using native currency as
     /// payment.
     /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenId The unique identifier of the NFT token to be subscribed.
+    /// @param target The address of the contract implementing the access control
     /// @param duration The duration (in seconds) to extend the subscription for.
-    function updateSubscription(uint256 tokenId, uint64 duration) external payable;
+    /// @param tokenId The unique identifier of the NFT token to be subscribed.
+    function updateSubscription(address target, uint64 duration, uint256 tokenId) external payable;
 
     /// @notice Extends the subscription for the given `tokenIds` with a specified `duration`, using native currency as
     /// payment.
     /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
+    /// @param target The address of the contract implementing the access control
     /// @param duration The duration (in seconds) to extend the subscription for.
-    function updateSubscription(uint256[] calldata tokenIds, uint64 duration) external payable;
-
-    /// @notice Extends the subscription for the given `tokenIds` with specified `durations`, using native currency as
-    /// payment.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
     /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
-    /// @param durations An array of durations (in seconds) for which the subscriptions are to be extended.
-    function updateSubscription(uint256[] calldata tokenIds, uint64[] calldata durations) external payable;
-
-    /*//////   updateSubscriptionWithERC20Payment variants   /////*/
-
-    /// @notice Extends the subscription for the given `tokenId` with a specified `duration`, using the accepted ERC20
-    /// token as payment.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenId The unique identifier of the NFT token to be subscribed.
-    /// @param duration The duration (in seconds) to extend the subscription for.
-    /// @param erc20 The address of the ERC20 token to be used for payment.
-    function updateSubscriptionWithERC20Payment(uint256 tokenId, uint64 duration, address erc20) external;
-
-    /// @notice Extends the subscription for the given `tokenIds` with a specified `duration`, using the accepted ERC20
-    /// token as payment.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
-    /// @param duration The duration (in seconds) to extend the subscription for.
-    /// @param erc20 The address of the ERC20 token to be used for payment.
-    function updateSubscriptionWithERC20Payment(uint256[] calldata tokenIds, uint64 duration, address erc20) external;
-
-    /// @notice Extends the subscription for the given `tokenIds` with a specified `durations`, using the accepted ERC20
-    /// token as payment.
-    /// @dev This function is meant to be called by the minter when minting the NFT to subscribe.
-    /// @param tokenIds An array of unique identifiers of the NFT tokens to update the subscriptions for.
-    /// @param durations An array of durations (in seconds) for which the subscriptions are to be extended.
-    /// @param erc20 The address of the ERC20 token to be used for payment.
-    function updateSubscriptionWithERC20Payment(
-        uint256[] calldata tokenIds,
-        uint64[] calldata durations,
-        address erc20
-    )
-        external;
+    function updateSubscription(address target, uint64 duration, uint256[] calldata tokenIds) external payable;
 }
