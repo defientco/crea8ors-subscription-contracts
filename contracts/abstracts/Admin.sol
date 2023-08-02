d2dbef91f3e50becb18b1131168eda2a7a39ad7c
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-02 15:40:45 +0500

    refactor: remove cre8ors storage variable and use target param
    less redundant storage
    more reusability

--- contracts/abstracts/Admin.sol
+++ contracts/abstracts/Admin.sol
@@ -1,26 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { ICre8ors } from "@crea8ors/interfaces/ICre8ors.sol";
 import { IERC721Drop } from "@crea8ors/interfaces/IERC721Drop.sol";
 
 /// @title Admin
-/// @notice An abstract contract that restricts access to only the cre8orsNFT contract's admin.
+/// @notice An abstract contract that restricts access to only the admin.
 abstract contract Admin {
-    ///@notice The address of the collection contract that mints and manages the tokens.
-    address public cre8orsNFT;
-
-    /// @dev Modifier that restricts access to only the contract's admin.
-    modifier onlyAdmin() {
-        if (!ICre8ors(cre8orsNFT).isAdmin(msg.sender)) {
+    /// @notice Only allow for users with admin access
+    modifier onlyAdmin(address _target) {
+        if (!isAdmin(_target, msg.sender)) {
             revert IERC721Drop.Access_OnlyAdmin();
         }
+
         _;
     }
 
-    /// @dev Constructor for the Admin contract.
-    /// @param _cre8orsNFT The address of the Cre8ors NFT contract.
-    constructor(address _cre8orsNFT) {
-        cre8orsNFT = _cre8orsNFT;
+    /// @notice Getter for admin role associated with the contract to handle minting
+    /// @param user user address
+    /// @return boolean if address is admin
+    function isAdmin(address _target, address user) public view returns (bool) {
+        return IERC721Drop(_target).isAdmin(user);
     }
 }
