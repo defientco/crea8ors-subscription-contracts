191a61be44703c77e8de44b3a606ae47fd71ece3
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-08-03 00:54:44 +0500

    test: update basic integration test flow

--- sol_test/Subscription.t.sol
+++ sol_test/Subscription.t.sol
@@ -1,51 +1,59 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19 <0.9.0;
 
-// import { Test } from "forge-std/Test.sol";
-// import { MockERC721 } from "contracts/mocks/MockERC721.sol";
-// import { Subscription } from "contracts/Subscription.sol";
-// import { IERC721 } from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import { Test } from "forge-std/Test.sol";
 
-// // solhint-disable-next-line
-// import { console2 } from "forge-std/console2.sol";
+import { MockMinter } from "contracts/mocks/MockMinter.sol";
+import { MockNFT } from "contracts/mocks/MockNFT.sol";
+import { Subscription } from "contracts/Subscription.sol";
 
-// error SubscriptionExpired();
+// solhint-disable-next-line
+import { console2 } from "forge-std/console2.sol";
 
-// contract SubscriptionTest is Test {
-//     address payable internal user = payable(address(0x12345));
-//     MockERC721 internal mockERC721;
-//     Subscription internal subscription;
+error SubscriptionExpired();
 
-//     function setUp() public virtual {
-//         vm.label(user, "User");
-//         vm.deal(user, 10 ether);
-//         vm.startPrank(user);
+contract SubscriptionTest is Test {
+    address payable internal user = payable(address(0x12345));
+    MockMinter internal mockMinter;
+    MockNFT internal mockNFT;
+    Subscription internal subscription;
 
-//         mockERC721 = new MockERC721({enable: true});
+    function setUp() public virtual {
+        vm.label(user, "User");
+        vm.deal(user, 10 ether);
+        vm.startPrank(user);
 
-//         subscription = new Subscription({erc721_: IERC721(address(mockERC721))});
+        mockNFT = new MockNFT();
 
-//         MockERC721(mockERC721).setSubscription(address(subscription));
-//     }
+        subscription = new Subscription({
+            cre8orsNFT_: address(mockNFT),
+            minRenewalDuration_: 1 days,
+            pricePerSecond_: 38580246913 // Roughly calculates to 0.1 ether per 30 days
+        });
 
-//     function testBasicSubscriptionModel() external {
-//         uint256 tokenId = 1;
-//         address owner;
+        mockNFT.setSubscription(address(subscription));
+        mockNFT.toggleSubscription();
 
-//         vm.warp(block.timestamp + 1 days);
+        mockMinter = new MockMinter({_mockNFT: address(mockNFT), _subscription: address(subscription)});
+    }
 
-//         // mint nft for user(user)
-//         mockERC721.mintWithSubscription(user, tokenId, 2 days);
+    function test_BasicSubscriptionModel() external {
+        uint256 tokenId = 1;
+        address owner;
 
-//         // ownerOf returns correct user
-//         owner = mockERC721.ownerOf(tokenId);
-//         assertEq(owner, user);
+        vm.warp(block.timestamp + 1 days);
 
-//         // 30 days passed
-//         vm.warp(block.timestamp + 30 days);
+        // mint nft for user(user)
+        mockMinter.mint(address(mockNFT), user, tokenId);
 
-//         // ownerOf reverts with SubscriptionExpired
-//         vm.expectRevert(SubscriptionExpired.selector);
-//         owner = mockERC721.ownerOf(tokenId);
-//     }
-// }
+        // ownerOf returns correct user
+        owner = mockNFT.ownerOf(tokenId);
+        assertEq(owner, user);
+
+        // 30 days passed
+        vm.warp(block.timestamp + 30 days);
+
+        owner = mockNFT.ownerOf(tokenId);
+        assertEq(owner, address(0));
+    }
+}
